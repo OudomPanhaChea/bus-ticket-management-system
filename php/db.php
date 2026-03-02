d932853256c9355581d5fe84ffcb7f4873a16f1d
OudomPanhaChea <169124803+OudomPanhaChea@users.noreply.github.com>
2026-03-02 14:45:02 +0700

    php: update env

--- php/db.php
+++ php/db.php
@@ -1,11 +1,12 @@
 <?php
-$host = "localhost";
-$dbname = "bus_ticket_db";
-$username = "root";
-$password = ""; 
-
+$envPath = __DIR__ . "/../.env";
+if (!file_exists($envPath)) {
+    echo json_encode(["error" => "Cannot find .env at: " . $envPath]);
+    exit;
+}
+$env = parse_ini_file($envPath);
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo = new PDO("mysql:host={$env['DB_HOST']};dbname={$env['DB_NAME']};charset=utf8", $env['DB_USER'], $env['DB_PASS']);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
